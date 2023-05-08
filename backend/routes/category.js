be6dbc993d9e3acd8e613f3d13e3973bcbaf9ca9
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-05-08 10:52:40 +0200

    Testar nåt annat

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
-
-router.get("/category", (req, res) => {
-  res.send("Här visar vi kategorier");
+router.get("/category", async (req, res) => {
+  let sql = "SELECT * FROM category;";
+  try {
+    await connection.query(sql, function (error, results, fields) {
+      if (error) {
+        if (error) throw error;
+      }
+      res.json(results);
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
 });
 
 module.exports = router;
