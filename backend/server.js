c61167b643e96a1a6c6590f031a4bfba7d34c69d
CodeByJohan <johan.gotet@gmail.com>
2023-05-08 10:14:15 +0200

    Added express router unit

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql')
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+// const mysql = require("mysql");
+const bodyParser = require("body-parser");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-const unitRoutes = require('./routes/unit')
+const unitRoutes = require("./routes/unit");
+const amountRoutes = require("./routes/amount");
+const categoryRoutes = require("./routes/category");
+const ingredientsRoutes = require("./routes/ingredients");
+const recipeRoutes = require("./routes/recipe");
 
-app.use(unitRoutes)
+app.use(unitRoutes);
