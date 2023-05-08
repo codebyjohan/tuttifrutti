c01c4db282becd86f3e916f8491168c08f22bd0a
CodeByJohan <johan.gotet@gmail.com>
2023-05-08 10:35:06 +0200

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-// const mysql = require("mysql");
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-const amountRoutes = require('./routes/amount')
-const categoryRoutes = require('./routes/category')
-const ingredientsRoutes = require('./routes/ingredients')
-const recipeRoutes = require('./routes/recipe')
-const unitRoutes = require('./routes/unit')
+const unitRoutes = require("./routes/unit");
+const amountRoutes = require("./routes/amount");
+const categoryRoutes = require("./routes/category");
+const ingredientsRoutes = require("./routes/ingredients");
+const recipeRoutes = require("./routes/recipe");
 
-app.use(amountRoutes)
-app.use(categoryRoutes)
-app.use(ingredientsRoutes)
-app.use(recipeRoutes)
-app.use(unitRoutes)
+app.use(
+  unitRoutes,
+  amountRoutes,
+  categoryRoutes,
+  ingredientsRoutes,
+  recipeRoutes
+);
