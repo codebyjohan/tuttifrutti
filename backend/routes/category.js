b717415ea5e46320363e76e9db4be75c030ba8d3
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-05-08 11:45:25 +0200

    Nu ska det funka att få upp kategorier

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,29 +1,33 @@
-// const express = require("express");
-// const router = express.Router();
-
-// router.get("/category", async (req, res) => {
-//   let sql = "SELECT * FROM category;";
-//   try {
-//     await connection.query(sql, function (error, results, fields) {
-//       if (error) {
-//         if (error) throw error;
-//       }
-//       res.json(results);
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       error: error.message,
-//     });
-//   }
-// });
-
-// module.exports = router;
-
 const express = require("express");
 const router = express.Router();
+const connection = require('./../connection');
 
-router.get("/category", async (req, res) => {
-  res.send("Här visar vi kategorier");
+router.get('/category', async (req, res) => {
+  let sql = 'SELECT * FROM category'
+  try {
+    await connection.query(sql, function (error, results, fields) {
+      if (error) {
+        if (error) throw error;
+      }
+      res.json(results);
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
 });
 
 module.exports = router;
+
+// const express = require("express");
+// const router = express.Router();
+// const connection = require('./../connection')
+
+// router.get("/api/category", async (req, res) => {
+//   res.send("Här visar vi kategorier"),
+//   let sql = "SELECT * FROM category;";
+// });
+
+
+// module.exports = router;
