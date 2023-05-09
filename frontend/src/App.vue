471c9cf0c395f5d53f63df1ac1c2190696def2f9
ivajunu <ivanna.junuzovic@live.se>
2023-05-09 13:59:05 +0200

    added header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,35 +1,62 @@
-<script>
-  import CitiesList from './components/CitiesList.vue'
-  import CounterButton from './components/CounterButton.vue'
-  import HelloWorld from './components/HelloWorld.vue'
 
-  export default {
-    components: {
-      CitiesList,
-      CounterButton,
-      HelloWorld
-    }
-  }
-</script>
 
 <template>
+  <header>
+    <img src="/assets/Logga.png" alt="Sidans logga" />
   <nav>
     <ul>
       <li>
         <RouterLink to="/">Hem</RouterLink>
       </li>
       <li>
-        <RouterLink to="/about">Om</RouterLink>
+        <RouterLink to="/about">Kategorier</RouterLink>
       </li>
       <li>
-        <RouterLink to="/contact">Kontakt</RouterLink>
+        <RouterLink to="/contact">Recept</RouterLink>
       </li>
     </ul>
   </nav>
-  <HelloWorld msg="Hello World!" />
-  <CitiesList />
-  <CounterButton />
+</header>
   <main>
     <RouterView />
   </main>
+  <footer>
+    <p>
+    AJETI - DREAM TEAM
+    </p>
+  </footer>
 </template>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Inria Serif', serif;
+}
+img {
+  max-width: 100%;
+  width: 50px;
+}
+header {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  padding: 10px;
+  border-bottom: rgba(0,0,0,0.5) solid 2px;
+}
+
+li, a {
+  display: inline-block;
+  list-style-type: none;
+  text-decoration: none;
+  padding: 20px;
+  font-size: large;
+}
+footer {
+  display: flex;
+  justify-content: flex-end;
+  padding: 20px;
+}
+</style>
