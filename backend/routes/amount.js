8003358d28eb0b337aec2762b2f85b294252a6ed
ivajunu <ivanna.junuzovic@live.se>
2023-05-09 11:07:41 +0200

    amount

--- backend/routes/amount.js
+++ backend/routes/amount.js
@@ -19,3 +19,69 @@ router.get("/amount", async (req, res) => {
     });
   }
 });
+
+router.post("/amount", async (req, res) => {
+  let sql = "INSERT INTO amount (amountNumber) VALUES (?)";
+  let params = [req.body.amountNumber];
+  try {
+    await connection.query(sql, params, function (error, results, fields) {
+      if (error) {
+        if (error) throw error;
+      }
+      res.json(results);
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
+
+router.put("/amount", async (req, res) => {
+  let sql = "UPDATE amount SET amountNumber = ?  WHERE amountId = ?";
+  let params = [req.body.amountId, req.body.amountNumber];
+
+  try {
+    await connection.query(sql, params, function (error, results, fields) {
+      if (error) {
+        if (error) throw error;
+      }
+      return res.status(201).json({
+        success: true,
+        error: "",
+      });
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+router.delete("/amount", async (req, res) => {
+  console.log(req.body);
+  let sql = "DELETE FROM amount WHERE amountId = ?";
+
+  try {
+    await connection.query(
+      sql,
+      [req.body.amountId],
+      function (error, results, fields) {
+        if (error) {
+          if (error) throw error;
+        }
+        return res.status(201).json({
+          success: true,
+          error: "",
+          message: "Nummer i amount är nu raderad!",
+        });
+      }
+    );
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+});
