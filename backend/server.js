91325cf2887e6f6b3fc482cf1ddbb7fcd62abc72
emi-river <emi.riverdi@gmail.com>
2023-05-11 15:46:35 +0200

    Connected input with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,139 +1,108 @@
-const express = require('express')
-const cors = require('cors')
-const mysql = require('mysql')
-const bodyParser = require('body-parser')
-const mongoose = require("mongoose")
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const CommentsModel = require("./models")
+const CommentsModel = require("./models");
 
+const app = express();
+const port = 3000;
 
-const app = express()
-const port = 3000
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 const start = async () => {
   try {
-      await mongoose.connect(
-          "mongodb://127.0.0.1:27017/commentsmongoose"
-      )
+    await mongoose.connect("mongodb://127.0.0.1:27017/commentsmongoose");
   } catch (error) {
-      console.error(error)
-      process.exit(1)
+    console.error(error);
+    process.exit(1);
   }
-}
-start()
+};
+start();
 
 app.get("/comments", async (req, res) => {
   try {
-      const allComments = await CommentsModel.find()
-      return res.status(200).json(allComments)
+    const allComments = await CommentsModel.find();
+    return res.status(200).json(allComments);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
   }
-  catch (error) {
-      return res.status(500).json({
-          error: error.message
-      })
-  }
-})
+});
 
 app.post("/comments", async (req, res) => {
-  let commentText = req.body.commentText
-  let commentUser = req.body.commentUser
-  let commentDate = req.body.dateOfComment
-
-
+  let commentText = req.body.commentText;
+  let commentUser = req.body.commentUser;
+  let commentDate = req.body.dateOfComment;
 
   try {
-      const newComment = new CommentsModel({commentText:commentText, commentUser:commentUser, dateOfComment:commentDate})
-      const insertedComment = await newCommment.save()
-      return res.status(201).json(insertedComment)
+    const newComment = new CommentsModel({
+      commentText: commentText,
+      commentUser: commentUser,
+      dateOfComment: commentDate,
+    });
+    const insertedComment = await newComment.save();
+    return res.status(201).json(insertedComment);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
   }
-  catch (error) {
-      return res.status(500).json({
-          error: error.message
-      })
+});
+
+app.put("/comments", async (req, res) => {
+  let commentId = req.body._id;
+  let updateText = req.body.commentText;
+  let updateUser = req.body.commentUser;
+
+  try {
+    // await updatedComment.updateOne({
+    await CommentsModel.updateOne(
+      { _id: commentId },
+      {
+        //   _id: commentId,
+        commentText: updateText,
+        commentUser: updateUser,
+        // })
+      }
+    );
+    const updatedComment = await CommentsModel.find({ _id: commentId });
+
+    return res.status(200).json(updatedComment);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
   }
-})
+});
 
-// app.put("/api/books", async (req, res) => {
-//   let bookIsbn = req.body.bokIsbn
-//   let bookTitle = req.body.bokTitel
-//   let bookAuthor = req.body.bokForfattare
-//   let bookPrice = req.body.bokPris
-//   let bookCategory1 = req.body.bokKategori1
-//   let bookCategory2 = req.body.bokKategori2
+app.delete("/comments", async (req, res) => {
+  let commentId = req.body._id;
+  try {
+    const deletedComment = await CommentsModel.findByIdAndDelete(commentId);
+    return res.status(200).json(deletedComment);
+  } catch (error) {
+    return res.status(500).json({
+      error: error.message,
+    });
+  }
+});
 
-//   try {
-//       await BookModel.updateOne({isbn: bookIsbn}, {
-//           book: bookTitle,
-//           author: bookAuthor,
-//           isbn: bookIsbn,
-//           price: bookPrice,
-//           cateogries: [bookCategory1, bookCategory2]
-//       })
-//       const updatedBook = await BookModel.find({isbn: bookIsbn})
-//       return res.status(200).json(updatedBook)
-//   }
-//   catch (error) {
-//       return res.status(500).json({
-//           error: error.message
-//       })
-//   }
-// })
+const connection = require("./connection");
 
-// app.delete("/api/books", async (req, res) => {
-//   let bookIsbn = req.body.bokIsbn
-//   try {
-//       const deletedBook = await BookModel.deleteOne({isbn: bookIsbn})
-//       return res.status(200).json(deletedBook)
-//   }
-//   catch (error) {
-//       return res.status(500).json({
-//           error: error.message
-//       })
-//   }
-// })
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-// app.delete("/api/books-byid", async (req, res) => {
-//   let bookId = req.body.id
-//   try {
-//       const deletedBook = await BookModel.findByIdAndDelete(bookId)
-//       return res.status(200).json(deletedBook)
-//   }
-//   catch (error) {
-//       return res.status(500).json({
-//           error: error.message
-//       })
-//   }
-// })
-
-// app.get("/api/books-byid/:id", async (req, res) => {
-//   const currentId = req.params.id
-//   try {
-//       const book = await BookModel.findById(currentId);
-//       return res.status(200).json(book);
-//   }
-//   catch (error) {
-//       return res.status(500).json({
-//           error: error.message
-//       })
-//   }
-// })
-
-
-app.use(cors())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-
-const connection = require('./connection')
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
-const unitRoutes = require('./routes/unit')
-const amountRoutes = require('./routes/amount')
-const categoryRoutes = require('./routes/category')
-const ingredientsRoutes = require('./routes/ingredients')
-const recipeRoutes = require('./routes/recipe')
-const recipeIngredientsRoutes = require('./routes/recipeIngredients')
-const recipeCategoryRoutes = require('./routes/recipeCategory')
+const unitRoutes = require("./routes/unit");
+const amountRoutes = require("./routes/amount");
+const categoryRoutes = require("./routes/category");
+const ingredientsRoutes = require("./routes/ingredients");
+const recipeRoutes = require("./routes/recipe");
+const recipeIngredientsRoutes = require("./routes/recipeIngredients");
+const recipeCategoryRoutes = require("./routes/recipeCategory");
 
 app.use(
   unitRoutes,
@@ -143,4 +112,4 @@ app.use(
   recipeRoutes,
   recipeIngredientsRoutes,
   recipeCategoryRoutes
-)
+);
