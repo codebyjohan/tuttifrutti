82afb4c8507800b39c4bb0c0226169d012ff0c98
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-05-11 12:08:29 +0200

    worked on mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,122 @@ const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
+const mongoose = require("mongoose")
+
+const CommentsModel = require("./models")
+
 
 const app = express()
 const port = 3000
+
+const start = async () => {
+  try {
+      await mongoose.connect(
+          "mongodb://127.0.0.1:27017/commentsmongoose"
+      )
+  } catch (error) {
+      console.error(error)
+      process.exit(1)
+  }
+}
+start()
+
+app.get("/comments", async (req, res) => {
+  try {
+      const allComments = await CommentsModel.find()
+      return res.status(200).json(allComments)
+  }
+  catch (error) {
+      return res.status(500).json({
+          error: error.message
+      })
+  }
+})
+
+app.post("/comments", async (req, res) => {
+  let commentText = req.body.commentText
+  let commentUser = req.body.commentUser
+  let commentDate = req.body.dateOfComment
+
+
+
+  try {
+      const newComment = new CommentsModel({commentText:commentText, commentUser:commentUser, dateOfComment:commentDate})
+      const insertedComment = await newCommment.save()
+      return res.status(201).json(insertedComment)
+  }
+  catch (error) {
+      return res.status(500).json({
+          error: error.message
+      })
+  }
+})
+
+// app.put("/api/books", async (req, res) => {
+//   let bookIsbn = req.body.bokIsbn
+//   let bookTitle = req.body.bokTitel
+//   let bookAuthor = req.body.bokForfattare
+//   let bookPrice = req.body.bokPris
+//   let bookCategory1 = req.body.bokKategori1
+//   let bookCategory2 = req.body.bokKategori2
+
+//   try {
+//       await BookModel.updateOne({isbn: bookIsbn}, {
+//           book: bookTitle,
+//           author: bookAuthor,
+//           isbn: bookIsbn,
+//           price: bookPrice,
+//           cateogries: [bookCategory1, bookCategory2]
+//       })
+//       const updatedBook = await BookModel.find({isbn: bookIsbn})
+//       return res.status(200).json(updatedBook)
+//   }
+//   catch (error) {
+//       return res.status(500).json({
+//           error: error.message
+//       })
+//   }
+// })
+
+// app.delete("/api/books", async (req, res) => {
+//   let bookIsbn = req.body.bokIsbn
+//   try {
+//       const deletedBook = await BookModel.deleteOne({isbn: bookIsbn})
+//       return res.status(200).json(deletedBook)
+//   }
+//   catch (error) {
+//       return res.status(500).json({
+//           error: error.message
+//       })
+//   }
+// })
+
+// app.delete("/api/books-byid", async (req, res) => {
+//   let bookId = req.body.id
+//   try {
+//       const deletedBook = await BookModel.findByIdAndDelete(bookId)
+//       return res.status(200).json(deletedBook)
+//   }
+//   catch (error) {
+//       return res.status(500).json({
+//           error: error.message
+//       })
+//   }
+// })
+
+// app.get("/api/books-byid/:id", async (req, res) => {
+//   const currentId = req.params.id
+//   try {
+//       const book = await BookModel.findById(currentId);
+//       return res.status(200).json(book);
+//   }
+//   catch (error) {
+//       return res.status(500).json({
+//           error: error.message
+//       })
+//   }
+// })
+
 
 app.use(cors())
 app.use(bodyParser.json())
