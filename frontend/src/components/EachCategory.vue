3b4ebb1a0106951226b301d20e71c45a2f0179d1
Bella Solo <bella.solomon-falkfrii@outlook.com>
2023-05-13 12:23:08 +0200

    Styling EachCategory

--- frontend/src/components/EachCategory.vue
+++ frontend/src/components/EachCategory.vue
@@ -1,39 +1,75 @@
+    <script>
+      export default {
+        created() {
+          fetch('http://localhost:3000/category_recipes')
+            .then((res) => res.json())
+            .then((data) => {
+              this.categoryRecipes = data
+            })
+        },
+        computed: {
+          OneCategory() {
+            if (this.categoryRecipes === null) {
+              return []
+            } else {
+              return this.categoryRecipes.filter(
+                (categories) =>
+                  categories.categoryName === this.$route.params.catName
+              )
+            }
+          }
+        },
+        data() {
+          return {
+            categoryRecipes: ''
+          }
+        }
+      }
+    </script>
+
+
 <template>
   <div>
     <h1>{{ $route.params.catName }}</h1>
   </div>
 
-  <div v-for="recipes in OneCategory" :key="recipes.recipeCategoryId">
-    {{ recipes.recipeName }}
-    <img :src="recipes.recipeImage" :alt="recipes.recipeName" />
+  <div class="recipeContainer">
+    <div class="theRecipe" v-for="recipes in OneCategory" :key="recipes.recipeCategoryId">
+      <!-- <router-link :to="`/recipe/${recipe.recipeId}`"> -->
+      <img :src="recipe.recipeImage" alt="Bild på maträtt" />
+      <h2>
+       {{ recipe.recipeName }}
+    </h2>
+    <!-- </router-link> -->
+    </div>
   </div>
 </template>
 
-<script>
-  export default {
-    created() {
-      fetch('http://localhost:3000/category_recipes')
-        .then((res) => res.json())
-        .then((data) => {
-          this.categoryRecipes = data
-        })
-    },
-    computed: {
-      OneCategory() {
-        if (this.categoryRecipes === null) {
-          return []
-        } else {
-          return this.categoryRecipes.filter(
-            (categories) =>
-              categories.categoryName === this.$route.params.catName
-          )
-        }
-      }
-    },
-    data() {
-      return {
-        categoryRecipes: ''
-      }
-    }
-  }
-</script>
+<style scoped>
+h1,
+h2 {
+  text-align: center;
+}
+
+
+.recipeContainer{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+}
+
+.theRecipe{
+display: flex;
+flex-direction: column;
+margin: 10px;
+}
+
+img{
+  height: 25vh;
+  padding: 5px;
+  width: 35vw;
+}
+
+
+</style>
