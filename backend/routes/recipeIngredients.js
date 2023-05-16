353ed03526795e62e514cb69a90c78f3ca2d6637
Annie Sahlén <annieimsahlen@gmail.com>
2023-05-16 09:49:17 +0200

    Correct misspelling

--- backend/routes/recipeIngredients.js
+++ backend/routes/recipeIngredients.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const router = express.Router();
-const connection = require("./../connection");
-module.exports = router;
+const express = require('express')
+const router = express.Router()
+const connection = require('./../connection')
+module.exports = router
 
-router.post("/recipeIngredients", async (req, res) => {
+router.post('/recipeIngredients', async (req, res) => {
   let sql =
-    "INSERT INTO recipeIngredients (recipeIngredientsRecId, recipeIngredientsIngId, recipeIngredientsUniId, recipeIngredientsAmoId) VALUES (?,?,?,?)";
+    'INSERT INTO recipeIngredients (recipeIngredientsRecId, recipeIngredientsIngId, recipeIngredientsUniId, recipeIngredientsAmoId) VALUES (?,?,?,?)'
   let params = [
     req.body.recipeIngredientsRecId,
     req.body.recipeIngredientsIngId,
     req.body.recipeIngredientsUniId,
     req.body.recipeIngredientsAmoId,
-  ];
+  ]
   try {
     await connection.query(sql, params, function (error, results, fields) {
       if (error) {
-        if (error) throw error;
+        if (error) throw error
       }
       return res.status(201).json({
         success: true,
-        error: "",
-        messgae: "Du har uppdaterat mellantabellen!",
-      });
-    });
+        error: '',
+        message: 'Du har uppdaterat mellantabellen!',
+      })
+    })
   } catch (error) {
     return res.status(500).json({
       success: false,
       error: error.message,
-    });
+    })
   }
-});
+})
